a324d92dc9704dcaed06ee26e26f72bbd0eceb62
Arshad <arshadpinjari666@gmail.com>
2021-05-23 17:33:56 +0530

    Added.QueueLinkedList_Enqueue

---
+++ QueueLinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QueueLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structure Using Queue Linked List");
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
+        }
+    }
+}
