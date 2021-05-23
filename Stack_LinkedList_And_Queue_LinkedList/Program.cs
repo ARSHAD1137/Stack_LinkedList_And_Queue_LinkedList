86b8e0ab9f9c777fa13704601ce4dcfb3f024d6c
Arshad <arshadpinjari666@gmail.com>
2021-05-23 17:18:01 +0530

    Add project files.

---
+++ Stack_LinkedList_And_Queue_LinkedList/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Stack_LinkedList_And_Queue_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data structure Using Stack Linked List");
+            
+        }
+    }
+}
