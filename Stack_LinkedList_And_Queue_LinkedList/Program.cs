cd154ba4cabf00d8b32e0fcc62d4f3741e27418c
Arshad <arshadpinjari666@gmail.com>
2021-05-23 17:22:28 +0530

    Added.LinkedList

--- Stack_LinkedList_And_Queue_LinkedList/Program.cs
+++ Stack_LinkedList_And_Queue_LinkedList/Program.cs
@@ -7,7 +7,14 @@ namespace Stack_LinkedList_And_Queue_LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Data structure Using Stack Linked List");
-            
+            Console.WriteLine("Hello World!");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+            Console.ReadKey();
+
         }
     }
 }
